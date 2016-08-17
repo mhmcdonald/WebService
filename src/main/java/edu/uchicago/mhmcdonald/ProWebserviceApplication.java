e44abeaf720b9784267d6db77cf4bab0a9f02180
hiesenburg88 <mark.harrison.mcdonald@gmail.com>
2016-08-17 12:14:45 -0500

    init commit

---
+++ src/main/java/edu/uchicago/mhmcdonald/ProWebserviceApplication.java
@@ -0,0 +1,12 @@
+package edu.uchicago.mhmcdonald;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProWebserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProWebserviceApplication.class, args);
+	}
+}
